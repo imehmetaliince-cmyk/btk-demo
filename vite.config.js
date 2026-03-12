ef9059a33bc0b1d984ff7611311dc8ef99f8b5f4
Mehmet Ali İnce <imehmetaliince@gmail.com>
2026-03-12 13:30:53 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: 8080,              // Dışardan erişim için port
+    host: true,              // Railway public host için gerekli
+    allowedHosts: ['btk-demo-production.up.railway.app'] // Railway URL'ni buraya ekle
+  }
 })
